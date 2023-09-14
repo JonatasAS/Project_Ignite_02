699cafac0550e151a05aa046c83499cee4726f8b
JonatasAS <jonatas23997@live.com>
2023-09-13 22:46:24 -0300

    styles and config ESlint

--- ignite-focus/src/App.tsx
+++ ignite-focus/src/App.tsx
@@ -1,7 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+
+import { defaultTheme } from './styles/theme/default'
+import { GlobalStyle } from './styles/theme/global'
+
 export function App() {
   return (
-    <div></div>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
-
-
---
+++ ignite-focus/src/components/Button.styles.ts
@@ -0,0 +1,19 @@
+import styled, { css } from 'styled-components'
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
+
+interface ButtonContainerProps {
+  variant: ButtonVariant
+}
+
+const buttonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
+`
