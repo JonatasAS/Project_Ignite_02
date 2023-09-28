e20494f63d122f40ce512101b060babd7238766f
JonatasAS <jonatas23997@live.com>
2023-09-27 22:18:28 -0300

    validation with zod for form

--- ignite-focus/src/pages/Home/index.jsx
+++ ignite-focus/src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import { Play } from 'phosphor-react'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   CountdownContainer,
@@ -12,10 +14,24 @@ import {
   TaskInput,
 } from './styles'
 
-export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe à tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+})
 
-  function handleSubmitNewCycle() { }
+export function Home() {
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
+
+  function handleSubmitNewCycle(data) {
+    console.log(data)
+  }
+
+  console.log(formState.errors)
 
   const task = watch('task')
   const isSubmitDisabled = !task
